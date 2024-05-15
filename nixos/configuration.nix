2f57dfa75cbb3d4de984c2e3d3fe9bf8588365ed
Adar Dagan <AdarDagan@outlook.com>
2024-05-15 15:44:22 +0300

    24 current  2024-05-15 15:44:18  23.11.7093.44072e24566c  6.1.90                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -128,4 +128,14 @@
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "greeter";
+      };
+    };
+  };
 }
