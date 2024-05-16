70341985f769c4140d9ad06a6f9ccf3f8ced9764
Adar Dagan <AdarDagan@outlook.com>
2024-05-16 10:18:02 +0300

    Changes

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -8,7 +8,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    /etc/nixos/hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
   # Bootloader.
@@ -18,10 +18,6 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
