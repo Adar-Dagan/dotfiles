0385b7804ffd6d99caffaa8cbfcb6cba15aa5f53
Adar Dagan <AdarDagan@outlook.com>
2024-05-16 10:19:18 +0300

    41 current  2024-05-16 09:01:25  23.11.7093.44072e24566c  6.1.90                          *

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -71,14 +71,6 @@
     brave
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
