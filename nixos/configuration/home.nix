16e781792b1c43b2b182211b12933fb2d23eb5f4
Adar Dagan <AdarDagan@outlook.com>
2024-05-17 10:16:52 +0300

    68 current  2024-05-17 10:16:46  23.11.20240514.9ddcaff   6.1.90                          *

---
+++ nixos/configuration/home.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # TODO please change the username & home directory to your own
+  home.username = "adar";
+  home.homeDirectory = "/home/adar";
+
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.gnome.adwaita-icon-theme;
+    size = 32;
+    x11 = {
+      enable = true;
+      defaultCursor = "Adwaita";
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
