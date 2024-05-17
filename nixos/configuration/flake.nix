86495dbd504a97d3b05a3413a82627f064a4fbb9
Adar Dagan <AdarDagan@outlook.com>
2024-05-17 13:49:52 +0300

    79 current  2024-05-17 13:49:45  23.11.20240514.9ddcaff   6.1.90                          *

--- nixos/configuration/flake.nix
+++ nixos/configuration/flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
