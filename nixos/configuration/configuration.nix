6d88313525975338da08c47a9d41e89e60adf540
Adar Dagan <AdarDagan@outlook.com>
2024-10-18 09:11:08 +0300

    191 current  2024-10-18 09:11:05  24.11.20241014.a3c0b3b  6.6.56                          *

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -214,4 +214,10 @@
     dates = "07:00";
     randomizedDelaySec = "45min";
   };
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 }
