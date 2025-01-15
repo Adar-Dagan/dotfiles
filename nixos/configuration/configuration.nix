ffd589f4079b74f2d5ec066fb3c902752573994e
Adar Dagan <AdarDagan@outlook.com>
2025-01-15 15:19:28 +0200

    204 current  2025-01-15 15:19:25  25.05.20250113.9abb87b  6.6.71                          *

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -229,7 +229,6 @@
   programs.nix-ld.libraries = with pkgs; [
     # Add any missing dynamic libraries for unpackaged programs
     # here, NOT in environment.systemPackages
-    libglibutil
   ];
 
   services.blueman.enable = true;
