cf3fbc937371469bd9eb1e9547ff8c90089d4bfa
Adar Dagan <AdarDagan@outlook.com>
2025-05-01 16:31:31 +0300

    223 current  2025-05-01 16:31:28  25.05.20250429.46e634b  6.12.25                          *

--- nixos/configuration/flake.nix
+++ nixos/configuration/flake.nix
@@ -45,7 +45,7 @@
         }: {
           system.autoUpgrade = {
             enable = true;
-            flake = self.outPath;
+            flake = "/etc/nixos/flake.nix";
             flags = [
               "--update-input"
               "nixpkgs"
