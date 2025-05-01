798cd06f98d9581dba535dee9b339a14734b5204
Adar Dagan <AdarDagan@outlook.com>
2025-05-01 13:57:10 +0300

    216 current  2025-05-01 13:57:07  25.05.20250429.46e634b  6.12.25                          *

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -232,9 +232,9 @@
 
     script = let
       nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
-      nix-flake = "${config.system.build.nixos-rebuild}/bin/nix";
+      # nix-flake = "${config.system.build.nix}/bin/nix";
     in ''
-      ${nix-flake} flake update /etc/nixos/flake.nix
+      nix flake update /etc/nixos/flake.nix
       ${nixos-rebuild} switch --flake /etc/nixos/flake.nix
     '';
   };
