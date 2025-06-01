f14ca389de9a8f915189bbd8eff7bf696e614183
Adar Dagan <AdarDagan@outlook.com>
2025-06-01 07:57:30 +0300

    236 current  2025-06-01 07:57:24  25.11.20250528.96ec055  6.12.30                          *

--- nixos/configuration/configuration.nix
+++ nixos/configuration/configuration.nix
@@ -203,6 +203,7 @@
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
+
   nix.settings.auto-optimise-store = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
